b6bb4f48369d1430bd0a79039d1b7648ed9e4319
Lemons Wang <lemons.wang@trantect.com>
2016-03-17 16:52:37 +0800

    modify something

--- app/constant.js
+++ app/constant.js
@@ -1,3 +1,3 @@
-export const ACCESS_KEY = "OxkUCE2MPXBIDY2LlUOH8IzcYBxyiy1DZc53kJan";
-export const SECRET_KEY = "7sKHlCddau4kLtLO0qfV8d14ZF5C0PxqXIzARqcC";
+export const ACCESS_KEY = "";
+export const SECRET_KEY = "";
 export const HOST = "https://yunbi.com";

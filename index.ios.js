070c8882f06bb36ef40f64ada6be423f76ec12a0
Qianyi Wang <wangqianyi.cs@gmail.com>
2016-03-22 20:37:55 +0800

    remove listview component to CustomListView.js

--- index.ios.js
+++ index.ios.js
@@ -5,105 +5,45 @@
 'use strict';
 
 import React, {
+    NavigatorIOS,
     AppRegistry,
     Component,
     StyleSheet,
-    Text,
     View,
-    ListView
 } from 'react-native';
-import {YunBi} from "./app/yunbi";
 
-import {
-    ACCESS_KEY,
-    SECRET_KEY,
-    HOST} from "./app/constant";
+import {CustomListView} from './app/CustomListView';
 
 class EthereumPrice extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2)=> row1 !== row2
-            }),
-            loaded: false
-        };
     }
 
     componentDidMount() {
-        let yunbi = new YunBi(ACCESS_KEY, SECRET_KEY, HOST);
-        yunbi.getMarkets()
-            .then((response)=> {
-                console.log(response);
-                this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(response),
-                    loaded: true
-                });
-            })
-            .done();
+
     }
 
 
-    renderLoadingView() {
-        return (
-            <View style={styles.container}>
-                <Text>
-                    Loading data.....
-                </Text>
-            </View>
-        );
-    }
-
-    renderCoin(coin) {
-        return (
-            <View style={styles.container}>
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{coin.id}</Text>
-                    <Text style={styles.name}>{coin.name}</Text>
-                </View>
-            </View>
-        );
-    }
-
-
-    render() {
-        if (!this.state.loaded) {
-            return this.renderLoadingView();
-        }
-
-        return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={this.renderCoin}
-                style={styles.listView}>
-            </ListView>
-        );
+    render(){
+        return(
+                <NavigatorIOS
+                    style={styles.container}
+                    initialRoute={{
+                        title:"EthereumPrice",
+                        component: CustomListView
+                    }}>
+                </NavigatorIOS>
+            );
     }
 }
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
-    },
-    listView: {
-        paddingTop: 100,
-        backgroundColor: '#613030',
-    },
-    rightContainer: {
-        flex: 1,
-    },
-    name: {
-        textAlign: 'left',
-    },
+
     instructions: {
         textAlign: 'center',
         color: '#333333',

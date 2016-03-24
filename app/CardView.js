fdaca2c1cca50b548a49434aa03807298572ac4d
Qianyi Wang <wangqianyi.cs@gmail.com>
2016-03-24 22:18:46 +0800

    simple UI  show last price

--- app/CardView.js
+++ app/CardView.js
@@ -10,20 +10,51 @@ import React,{
     Component
 } from 'react-native';
 
+import {YunBi} from './yunbi';
+import {ACCESS_KEY,
+        SECRET_KEY,
+        HOST} from './constant';
+
 export class CardView extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            coinName:"",
+            coinPrice: "",
 
+        }
+    }
+
+
+    componentDidMount(){
+        let marketId = this.props.rowData.id;
+        let yunbi = new YunBi(ACCESS_KEY, SECRET_KEY, HOST);
+        yunbi.getTickersByMarket(marketId)
+            .then((response)=> {
+                this.setState({
+                    coinName:this.props.rowData.name,
+                    coinPrice:response.ticker.last,
+                })
+            });
     }
 
     render(){
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>
-                    Loading data.....
+                    {this.state.coinName}
+                    {this.state.coinPrice}
                 </Text>
             </View>
         )
     }
-
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center',
+        justifyContent: 'center',
+    }
+});

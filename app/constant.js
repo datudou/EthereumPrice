fdeb7ae18d956d2f02d5ced320571b7ac97918b1
Qianyi Wang <wangqianyi.cs@gmail.com>
2016-04-21 23:25:54 +0800

    delete semi

--- app/constant.js
+++ app/constant.js
@@ -1,3 +1,4 @@
-export const ACCESS_KEY = "";
-export const SECRET_KEY = "";
-export const HOST = "https://yunbi.com";
+export const ACCESS_KEY = ''
+export const SECRET_KEY = ''
+export const HOST = 'https://yunbi.com'
+
